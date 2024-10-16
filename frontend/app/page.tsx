c9dc790f93f79b3cf035104d1779c4da32d769e0
user <georgeadelin.aniei@kuleuven.student.be>
2024-10-16 12:27:45 +0200

    Website setup.

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react';
+
+const Home: React.FC = () => { 
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">Hello, World!</h1>
+        <p>Pula in cur</p>
+  </div>
+  );
+};
+
+export default Home;
